65ab3e2c92de6846d7c73aa03b3d4ec72cfa24ee
vedavyas90 <vedavyas90@gamil.com>
2015-04-17 17:21:11 +0530

    retrieving true or false through recursion

--- NetBeansProjects/PROLOG-EBG/src/prolog/ebg/PROLOGEBG.java
+++ NetBeansProjects/PROLOG-EBG/src/prolog/ebg/PROLOGEBG.java
@@ -7,41 +7,38 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
  * @author vyasa
  */
+   
 
 public class PROLOGEBG {
-        
-        
-    
-   // public boolean goalcheck(String ngoal) {
-   //     System.out.println("Entered goal check");
-   // }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException{
-        // TODO code application logic here
-        
-        System.out.println("Welcome to EBG");
-        Scanner scn=new Scanner(System.in);
+
+     
+     
         ArrayList<String> Rules =new ArrayList<String>();
         ArrayList<String> Facts =new ArrayList<String>();
         ArrayList<String> Goals =new ArrayList<String>();
+        Map<String,ArrayList<ArrayList<String>>> prules=new HashMap<String,ArrayList<ArrayList<String>>>();
+        Set<String> Functions=new HashSet<String>();
+        Set<String> Constants =new HashSet<String>();
+        ArrayList<String> Variables =new ArrayList<String>();
         
-        ArrayList<String> Functions=new ArrayList<String>();
-        
-        
-        System.out.println("Stage one on: Loading the Knowledge Base .....");
+    
+    public PROLOGEBG(String ipfile) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     System.out.println("Stage one on: Loading the Knowledge Base .....");
         
         try {
-			File file = new File("/home/vyasa/NetBeansProjects/PROLOG-EBG/src/prolog/ebg/input.txt");
+			System.out.println("Welcome to EBG");
+                        File file = new File(ipfile);
 			FileReader fileReader = new FileReader(file);
                         BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
@@ -95,17 +92,19 @@ public class PROLOGEBG {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+    }
         
-        
+    private void processRules() {
         
         String[] LRtemp,temp;
         String temp2=null;
         String Head=null;
         int i=0,k=0;//counts the number of rules with same head.
-        Map<String,ArrayList<ArrayList<String>>> prules=new HashMap<String,ArrayList<ArrayList<String>>>();
+        
         ArrayList<ArrayList<String>> Tails=new ArrayList<ArrayList<String>>();
         ArrayList<String> Tail=new ArrayList<String>();
-        
+
         System.out.println("Stage 2: processing rules....");
         
         for(String R : Rules){
@@ -170,14 +169,14 @@ public class PROLOGEBG {
             for(i=0;i<prules.get(Head).size();i++)
                 System.out.println("---> "+prules.get(Head).get(i));
         }
-        /*
-        String fun1 = Head.substring(0, Head.indexOf('('));//"Mortal";  
-            Functions.add(fun1);
-            System.out.println(fun1);
-            Tail.clear();
-        */
+        
+    }        
+    
+    private void constGen() {
+         
         System.out.println("Stage 3: determining constants and variables....");
-        ArrayList<String> Constants =new ArrayList<String>();
+        
+        
         for(String s:Facts){
         String con=s.substring(s.indexOf('(')+1, s.indexOf(')'));//"Ram","Sita";
         Constants.add(con);
@@ -186,17 +185,24 @@ public class PROLOGEBG {
         for(String c:Constants){
             System.out.println(c);
         }
-        
-        ArrayList<String> Variables =new ArrayList<String>();
         String Var1="X";
-
         Variables.add(Var1); 
-        
+    }
+     private boolean solveGoals(ArrayList<String> tempGoals) {
+        ArrayList<String> newGoals=new ArrayList<String>();
+        boolean flag=true;
+        /*
         System.out.println("Stage 4: Goal solving");
         int flag=1;
         System.out.println("Goals stored are:"); 
-        for(String g: Goals){   
-            System.out.println(g);
+        for(String g: tempGoals){
+            //goalCheck(g);
+            if(g.substring(g.indexOf("(")+1, g.indexOf(")")).matches("[A-Z][a-zA-Z]*")){
+                  System.out.println("answering query:"+g);
+            }
+            else if(g.substring(g.indexOf("(")+1, g.indexOf(")")).matches("[a-z][a-zA-Z]*")){
+                  System.out.println("Yes or No query:"+g);  
+                    }    
         }
         System.out.println("Facts stored are:"); 
         for(String f: Facts){   
@@ -204,13 +210,18 @@ public class PROLOGEBG {
         }
         System.out.println("Goal Tree");
         System.out.println("L0:Root");
-        System.out.println("L1:");
-        for(String g: Goals){   
+        System.out.println("L1:primary goals");
+        for(String g: tempGoals){   
             System.out.println(g);
         }
-        for(String g: Goals){  
-          if(!Facts.contains(g)){
-                //System.out.println("you cant obtain this goal directly from given facts");    
+        */
+        for(String g: tempGoals){  
+            if(Facts.contains(g)){
+                System.out.println("direct goal ,matching fact found :"+g);
+                flag= true;    
+            }
+            else if(!Facts.contains(g)){
+                System.out.println("you cant obtain this goal directly from given facts: "+g);    
                 String funct=g.substring(0,g.indexOf("("));
                 String cons=g.substring(g.indexOf("(")+1,g.indexOf(")"));
                 
@@ -225,7 +236,6 @@ public class PROLOGEBG {
                       ruleKey=str;
                   }
                 }
-                
                 if(prules.containsKey(ruleKey))
                 {
                     
@@ -240,27 +250,77 @@ public class PROLOGEBG {
                         for(int n=0;n<premises.get(j).size();n++){
                             String var=premises.get(j).get(n).substring(premises.get(j).get(n).indexOf("(")+1, premises.get(j).get(n).indexOf(")"));
                             String ngoal=premises.get(j).get(n);
-                            System.out.println(ngoal.replaceAll(var,cons)+" AND");
-                           // if(goalcheck(ngoal)){Facts.add(ngoal);}
+                            newGoals.add(ngoal.replaceAll(var,cons));
+                            flag= solveGoals(newGoals);
+                             System.out.println(newGoals);
+                             newGoals.clear();
+                           //if(goalcheck(ngoal)){Facts.add(ngoal);}
                         }
                         System.out.println("OR");
                        
                     }
+                   
                  }
-                flag=0;
+                else if(!prules.containsKey(ruleKey)){
+                System.out.println("No fact found and No Rule Found:"+g);
+                return true;    
+                }
+                
+                
             }
         }
-        if(flag==0)
-        { 
-            System.out.println("FALSE");
-      
+//        if(flag==0)
+//        { 
+//            System.out.println("FALSE");
+//      
+//        }
+//        else
+//            System.out.println("TRUE");
+        return flag;
+        }
+
+        public boolean goalcheck(String ngoal,ArrayList<String> goals) {
+            System.out.println("Entered goal check");
+        return false;
+   }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        
+        //Scanner scn=new Scanner(System.in);    
+        System.out.println("\n\n*********************************************************************");
+        
+        PROLOGEBG pebg=new PROLOGEBG("/home/vyasa/NetBeansProjects/PROLOG-EBG/src/prolog/ebg/input.txt");
+               
+        
+        System.out.println("\n\n*********************************************************************\n\n");
+        pebg.processRules();
+        System.out.println("\n\n*********************************************************************");
+        pebg.constGen();
+        System.out.println("\n\n*********************************************************************");
+        
+        if(pebg.solveGoals(pebg.Goals)){
+            System.out.println("TRUE");   
         }
         else
-            System.out.println("TRUE");
+            System.out.println("FALSE");
+//            String fun1 = Head.substring(0, Head.indexOf('('));//"Mortal";  
+//            Functions.add(fun1);
+//            System.out.println(fun1);
+//            //Tail.clear();
+
+//     
+        
         
        
         
     }  
+
+   
+
+
     
 }
 
